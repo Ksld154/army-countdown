39b6ec82292998bd8b6e424edc6922e033eb5ae6
Alex Lin <lylin1997.cs09g@nctu.edu.tw>
2023-01-28 18:45:49 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Army Countdown Timer
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Ksld154/pen/oNMyeaz](https://codepen.io/Ksld154/pen/oNMyeaz).
+
---
+++ src/script.js
@@ -0,0 +1,66 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let body = document.body;
+  body.classList.toggle('light-theme');
+  
+  // Unix timestamp (in seconds) to count down to
+  let targetTime = moment.tz("2023-03-29 17:00", "Asia/Taipei").unix();
+  
+  // Set up FlipDown
+  var flipdown = new FlipDown(targetTime, {
+    theme: "light",
+    headings: ["", "", "", ""],
+  })
+
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+      console.log('The countdown has ended!');
+    });
+  
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Grab the current date
+  let currentDate = new Date();
+
+  // calculate date progress
+  let startDate  = moment.tz("2022-12-06 00:00", "Asia/Taipei");
+  let targetDate = moment.tz("2023-03-29 17:00", "Asia/Taipei");
+  let totalDuration = Math.round((targetDate - startDate) / 1000);
+
+  let passedTime = (currentDate - startDate) / 1000;
+  let percent = (passedTime / totalDuration) * 100;
+  let roundedPercent = Math.round(percent * 1000) /1000;
+  console.log(roundedPercent);
+  
+  
+  // draw pie chart
+  let chart = document.getElementById("count-down");
+  chart.setAttribute("data-percent", roundedPercent);
+  chart.textContent = roundedPercent + "%";
+  
+  new EasyPieChart(chart, {
+    size: 150,
+    barColor: function(percent) {
+      var ctx = this.renderer.getCtx();
+      var canvas = this.renderer.getCanvas();
+      var gradient = ctx.createLinearGradient(0,0,canvas.width,0);
+      gradient.addColorStop(0, "#00f7ff");
+      gradient.addColorStop(1, "#ff00c4");
+      return gradient;
+    },
+    scaleLength: 0,
+    lineWidth: 15,
+    trackColor: "#373737",
+    lineCap: "circle",
+    animate: 2000,    
+  });
+  
+});
+
+
+
